83a7a3ebdc15f5cb6153930eb60ddb9fac8063bf
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2021-09-22 10:55:57 -0500

    Init project

---
+++ src/main/java/com/everisbootcamp/accountdeposit/AccountDepositApplication.java
@@ -0,0 +1,13 @@
+package com.everisbootcamp.accountdeposit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountDepositApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountDepositApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/everisbootcamp/accountdeposit/AccountDepositApplicationTests.java
@@ -0,0 +1,13 @@
+package com.everisbootcamp.accountdeposit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountDepositApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
