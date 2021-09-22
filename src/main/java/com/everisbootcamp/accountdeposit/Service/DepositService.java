ed495371c9429380b6520c5559d756cedb152d59
kevinpalma20 <umb.kevsidorov@gmail.com>
2021-09-22 18:15:59 -0500

    Update service

--- src/main/java/com/everisbootcamp/accountdeposit/Service/DepositService.java
+++ src/main/java/com/everisbootcamp/accountdeposit/Service/DepositService.java
@@ -5,8 +5,10 @@ import java.util.Map;
 import com.everisbootcamp.accountdeposit.Constants.Constants;
 import com.everisbootcamp.accountdeposit.Data.Deposit;
 import com.everisbootcamp.accountdeposit.Interface.DepositRepository;
+import com.everisbootcamp.accountdeposit.Model.AccountModel;
 import com.everisbootcamp.accountdeposit.Model.DepositModel;
 import com.everisbootcamp.accountdeposit.Model.ResponseModel;
+import com.everisbootcamp.accountdeposit.Web.Consumer;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -22,6 +24,16 @@ public class DepositService {
     @Autowired
     private DepositRepository repository;
 
+    private ResponseEntity<AccountModel> findAccountByNumberAccount(String number) {
+        return Consumer.webclientAccount
+            .get()
+            .uri("/".concat(number))
+            .retrieve()
+            .onStatus(status -> status.value() == 404, clientResponse -> Mono.empty())
+            .toEntity(AccountModel.class)
+            .block();
+    }
+
     public Mono<ResponseEntity<Map<String, Object>>> BindingResultErrors(BindingResult bindinResult) {
         ResponseModel response = new ResponseModel(
             bindinResult.getAllErrors().stream().findFirst().get().getDefaultMessage().toString(),
@@ -32,10 +44,17 @@ public class DepositService {
     }
 
     public Mono<ResponseModel> save(String numberaccount, DepositModel model) {
-        HttpStatus status = HttpStatus.NOT_ACCEPTABLE;
+        HttpStatus status = HttpStatus.NOT_FOUND;
         String message = Constants.Messages.INVALID_DATA; 
 
-         
+        if (findAccountByNumberAccount(numberaccount).getBody() != null) {
+            status = HttpStatus.CREATED;
+            message = Constants.Messages.CORRECT_DATA;
+
+            //add amount at account.
+ 
+            repository.save( new Deposit(numberaccount, model.getAmount()) ).subscribe();
+        } 
 
         return Mono.just(new ResponseModel(message, status));
     }
