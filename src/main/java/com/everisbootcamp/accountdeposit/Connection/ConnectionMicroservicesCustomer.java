cec5e7a36dd2e586d4355db754d934053f0ae00a
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2022-01-01 15:00:38 -0500

    FEAT: Add class connection.

---
+++ src/main/java/com/everisbootcamp/accountdeposit/Connection/ConnectionMicroservicesCustomer.java
@@ -0,0 +1,18 @@
+package com.everisbootcamp.accountdeposit.Connection;
+
+import com.everisbootcamp.accountdeposit.Model.Response.ResponseCustomer;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ConnectionMicroservicesCustomer {
+
+    public ResponseEntity<ResponseCustomer> findCustomerById(String idcustomer) {
+        return Consumer.webClientCustomer
+                .get() // 61378daca5a222750f6658b4
+                .uri(UriBuilder -> UriBuilder.path("findById").queryParam("id", idcustomer).build())
+                .retrieve()
+                .toEntity(ResponseCustomer.class)
+                .block();
+    }
+}
