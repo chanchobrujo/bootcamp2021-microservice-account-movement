719d217e2d620eaa71ad2471acfda9fbc96df926
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2022-01-01 23:25:41 -0500

    REFACTOR: Add path for accounts microservices.

--- src/main/java/com/everisbootcamp/accountdeposit/Connection/Consumer.java
+++ src/main/java/com/everisbootcamp/accountdeposit/Connection/Consumer.java
@@ -6,5 +6,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 public class Consumer {
 
     public static final WebClient webClientCustomer = WebClient.create(Path.URLS.CUSTOMERS_PATH);
+    public static final WebClient webClientAccount = WebClient.create(Path.URLS.ACCOUNT_PATH);
     public static final WebClient webClientLogic = WebClient.create(Path.URLS.LOGIC_PATH);
 }
