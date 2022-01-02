f9c11a55657ab78eb76b33264307b51a1b1e4495
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2022-01-02 11:17:08 -0500

    REFACTOR: Update service.

--- src/main/java/com/everisbootcamp/accountdeposit/Service/Movement/DepositService.java
+++ src/main/java/com/everisbootcamp/accountdeposit/Service/Movement/DepositService.java
@@ -9,10 +9,8 @@ import com.everisbootcamp.accountdeposit.Model.Request.RequestUpdateBalance;
 import com.everisbootcamp.accountdeposit.Model.Response.Response;
 import com.everisbootcamp.accountdeposit.Service.AccountService;
 import java.util.Objects;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
@@ -20,9 +18,6 @@ public class DepositService {
 
     @Autowired
     private DepositRepository repository;
-
-    // @Autowired
-    // private RuleService ruleService;
 
     @Autowired
     private AccountService accountService;
@@ -35,44 +30,14 @@ public class DepositService {
         );
 
         if (verifyExistAccount) {
-            // if (!this.ruleService.verifyRules(numberaccount)) {
-            // response = new Response(MessagesError.MOVEMENT_DENIED);
-            // } else {
             Double balance = this.accountService.AmountAccount(numberaccount) + model.getAmount();
             RequestUpdateBalance modelBal = new RequestUpdateBalance(numberaccount, balance);
             System.err.println(this.accountService.updateBalanceAccount(modelBal));
 
             repository.save(new Deposit(numberaccount, model.getAmount())).subscribe();
             response = new Response(MessagesSuccess.SUCCESS_REGISTER);
-            // }
         }
 
         return Mono.just(response);
-    }
-
-    public Flux<Deposit> findAll() {
-        return repository.findAll();
-    }
-
-    public Flux<Deposit> findByNumberAccount(String numberaccount) {
-        return Flux.fromIterable(
-            repository
-                .findAll()
-                .toStream()
-                .filter(r -> r.getNumberaccount().equals(numberaccount))
-                .collect(Collectors.toList())
-        );
-    }
-
-    public Mono<Deposit> findById(String id) {
-        return repository.findById(id);
-    }
-
-    public Flux<Deposit> findByDatecreated(String date) {
-        return repository.findByDatecreated(null);
-    }
-
-    public Flux<Deposit> findByNumberaccount(String numberaccount) {
-        return repository.findByNumberaccount(numberaccount);
     }
 }
