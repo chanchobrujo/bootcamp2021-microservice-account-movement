8e7f7e82b0e1bf4974ef55f5e8bfa3bda1bd6645
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2022-01-01 23:27:39 -0500

    FEAT: Add model for responses and resquest.

--- src/main/java/com/everisbootcamp/accountdeposit/Model/Response/ResponseAccount.java
+++ src/main/java/com/everisbootcamp/accountdeposit/Model/Response/ResponseAccount.java
@@ -17,6 +17,7 @@ public class ResponseAccount {
 
     private String NumberAccount;
     private String TypeAccount;
+    private String idcustomer;
     private Double Amount;
 
     private Map<String, Object> Rules;
