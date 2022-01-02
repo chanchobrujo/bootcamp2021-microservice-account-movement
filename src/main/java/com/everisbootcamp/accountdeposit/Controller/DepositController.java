e7a987ff61fd2f5f8a136cdb48b5b0093a2e9449
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2022-01-02 11:16:00 -0500

    REFACTOR: Init two types movement.

--- src/main/java/com/everisbootcamp/accountdeposit/Controller/DepositController.java
+++ src/main/java/com/everisbootcamp/accountdeposit/Controller/DepositController.java
@@ -1,21 +1,18 @@
 package com.everisbootcamp.accountdeposit.Controller;
 
-import com.everisbootcamp.accountdeposit.Data.Deposit;
 import com.everisbootcamp.accountdeposit.Error.ResponseBindingResultErrors;
-import com.everisbootcamp.accountdeposit.Model.Request.DepositModel;
-import com.everisbootcamp.accountdeposit.Service.DepositService;
+import com.everisbootcamp.accountdeposit.Model.Request.RequestMovement;
+import com.everisbootcamp.accountdeposit.Service.TypeMovementService;
 import java.util.Map;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -23,34 +20,37 @@ import reactor.core.publisher.Mono;
 public class DepositController {
 
     @Autowired
-    private DepositService service;
+    private TypeMovementService service;
 
     @Autowired
     private ResponseBindingResultErrors responseBindingResultErrors;
 
-    @GetMapping("/")
-    public Mono<ResponseEntity<Flux<Deposit>>> findByAll() {
-        return Mono.just(ResponseEntity.ok().body(service.findAll()));
-    }
-
-    @GetMapping("/{numberaccount}")
-    public Mono<ResponseEntity<Flux<Deposit>>> findByNumberAccount(
-        @PathVariable("numberaccount") String numberaccount
-    ) {
-        return Mono.just(ResponseEntity.ok().body(service.findByNumberAccount(numberaccount)));
-    }
+    /**
+     * @GetMapping("/")
+     * public Mono<ResponseEntity<Flux<Deposit>>> findByAll() {
+     * return Mono.just(ResponseEntity.ok().body(service.findAll()));
+     * }
+     *
+     * @GetMapping("/{numberaccount}")
+     * public Mono<ResponseEntity<Flux<Deposit>>> findByNumberAccount(
+     * @PathVariable("numberaccount") String numberaccount
+     * ) {
+     * return
+     * Mono.just(ResponseEntity.ok().body(service.findByNumberAccount(numberaccount)));
+     * }
+     */
 
     @PostMapping("/save/{numberaccount}")
     public Mono<ResponseEntity<Map<String, Object>>> save(
         @PathVariable("numberaccount") String numberaccount,
-        @RequestBody @Valid DepositModel model,
+        @RequestBody @Valid RequestMovement model,
         BindingResult bindinResult
     ) {
         if (bindinResult.hasErrors()) return this.responseBindingResultErrors.BindingResultErrors(
                 bindinResult
             );
         return service
-            .save(numberaccount, model)
+            .initMovement(numberaccount, model)
             .map(
                 response -> {
                     return ResponseEntity.status(response.getStatus()).body(response.getResponse());
--- src/main/java/com/everisbootcamp/accountdeposit/Service/TypeMovementService.java
+++ src/main/java/com/everisbootcamp/accountdeposit/Service/TypeMovementService.java
@@ -1,6 +1,41 @@
 package com.everisbootcamp.accountdeposit.Service;
 
+import com.everisbootcamp.accountdeposit.Common.Utils;
+import com.everisbootcamp.accountdeposit.Constants.Enums.Messages.MessagesError;
+import com.everisbootcamp.accountdeposit.Constants.Enums.Types.TypeMovement;
+import com.everisbootcamp.accountdeposit.Model.Request.RequestMovement;
+import com.everisbootcamp.accountdeposit.Model.Response.Response;
+import com.everisbootcamp.accountdeposit.Service.Movement.DepositService;
+import com.everisbootcamp.accountdeposit.Service.Movement.RetireService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
-public class TypeMovementService {}
+public class TypeMovementService {
+
+    @Autowired
+    private DepositService depositService;
+
+    @Autowired
+    private RetireService retireService;
+
+    public Mono<Response> initMovement(String numberaccount, RequestMovement model) {
+        Response response = new Response(MessagesError.NOTFOUND_DATA);
+        String typeMovement = model.getTypemovement();
+
+        Boolean verifyDeposit = Utils.equalsOrContains(
+            typeMovement,
+            TypeMovement.DEPOSIT.getName()
+        );
+        Boolean verifyRetire = Utils.equalsOrContains(typeMovement, TypeMovement.RETIRE.getName());
+
+        if (verifyDeposit) {
+            return this.depositService.save(numberaccount, model);
+        } else if (verifyRetire) {
+            return this.retireService.save(numberaccount, model);
+        }
+
+        return Mono.just(response);
+    }
+}
