968146337079318d0ecc018843906f6e2099e1e1
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2022-01-03 15:52:23 -0500

    REFACTOR: Add notation service.

--- src/main/java/com/everisbootcamp/accountdeposit/Service/MovementSave.java
+++ src/main/java/com/everisbootcamp/accountdeposit/Service/MovementSave.java
@@ -8,7 +8,9 @@ import com.everisbootcamp.accountdeposit.Model.Response.Response;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class MovementSave {
 
     @Autowired
