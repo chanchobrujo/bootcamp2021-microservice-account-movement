3e30116efb537d40f40aaba105ff7210a9c80e2b
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2022-01-03 12:13:39 -0500

    REFACTOR: Rename carpet.

---
+++ src/main/java/com/everisbootcamp/accountdeposit/Service/MovementSave.java
@@ -0,0 +1,35 @@
+package com.everisbootcamp.accountdeposit.Service;
+
+import com.everisbootcamp.accountdeposit.Constants.Enums.Messages.MessagesError;
+import com.everisbootcamp.accountdeposit.Constants.Enums.Messages.MessagesSuccess;
+import com.everisbootcamp.accountdeposit.Data.Movement;
+import com.everisbootcamp.accountdeposit.Interface.MovementRepository;
+import com.everisbootcamp.accountdeposit.Model.Response.Response;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MovementSave {
+
+    @Autowired
+    private MovementRepository movementRepository;
+
+    public Response save(String TYPE, String NUM, Double AMOUNT) {
+        Response response = new Response();
+        try {
+            Movement movement = Movement
+                .builder()
+                .typemovement(TYPE)
+                .numberaccount(NUM)
+                .amount(AMOUNT)
+                .datecreated(LocalDateTime.now(ZoneId.of("America/Lima")))
+                .build();
+
+            movementRepository.save(movement).subscribe();
+            response = new Response(MessagesSuccess.SUCCESS_REGISTER);
+        } catch (Exception e) {
+            response = new Response(MessagesError.MOVEMENT_DENIED);
+        }
+        return response;
+    }
+}
