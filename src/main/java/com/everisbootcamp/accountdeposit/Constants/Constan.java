56058208d7593e8721216c32d444476beea1f87c
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2022-01-03 16:41:36 -0500

    REFACTOR: Updates constan.

--- src/main/java/com/everisbootcamp/accountdeposit/Constants/Constan.java
+++ src/main/java/com/everisbootcamp/accountdeposit/Constants/Constan.java
@@ -2,8 +2,10 @@ package com.everisbootcamp.accountdeposit.Constants;
 
 public class Constan {
 
-    public static final String DOUBLE_POINT = ":";
+    public static final String FORMATTER_DATE = "dd-MM-yyyy";
     public static final String DOUBLE_SLASH = "//";
+    public static final String DOUBLE_SLASH_REVERSE = "\\";
+    public static final String DOUBLE_POINT = ":";
     public static final String SLASH = "/";
     public static final String SCRIPT = "-";
     public static final String PLUS = "+";
