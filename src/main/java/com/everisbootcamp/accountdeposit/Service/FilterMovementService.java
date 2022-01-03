4839717b5f75e59674fe6cf960417ae817d6dfc7
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2022-01-03 16:41:56 -0500

    REFACTOR: Add date created for movement.

--- src/main/java/com/everisbootcamp/accountdeposit/Service/FilterMovementService.java
+++ src/main/java/com/everisbootcamp/accountdeposit/Service/FilterMovementService.java
@@ -1,7 +1,8 @@
 package com.everisbootcamp.accountdeposit.Service;
 
+import com.everisbootcamp.accountdeposit.Common.Utils;
 import com.everisbootcamp.accountdeposit.Interface.MovementRepository;
-import java.util.Date;
+import java.text.ParseException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,13 +12,21 @@ public class FilterMovementService {
     @Autowired
     private MovementRepository repository;
 
+    private Integer getMonth(String date) {
+        try {
+            return Utils.getMonth(date);
+        } catch (ParseException e) {
+            return -1;
+        }
+    }
+
     public int getMonthlyMovementsQuantity(String numberaccount) {
-        Date date = new Date();
-        Integer month = date.getMonth();
+        Integer month = this.getMonth(Utils.date());
+
         return (int) repository
             .findAll()
             .toStream()
-            .filter(mov -> mov.getDatecreated().getMonth().getValue() == month)
+            .filter(mov -> this.getMonth(mov.getDatecreated()) == month)
             .filter(d -> d.getNumberaccount().equals(numberaccount))
             .count();
     }
