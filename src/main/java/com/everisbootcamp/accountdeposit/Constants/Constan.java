1623bfce9319fef249b1028a695b7bf85e832a44
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2022-01-03 15:51:49 -0500

    REFACTOR: Constan.

---
+++ src/main/java/com/everisbootcamp/accountdeposit/Constants/Constan.java
@@ -0,0 +1,11 @@
+package com.everisbootcamp.accountdeposit.Constants;
+
+public class Constan {
+
+    public static final String DOUBLE_POINT = ":";
+    public static final String DOUBLE_SLASH = "//";
+    public static final String SLASH = "/";
+    public static final String SCRIPT = "-";
+    public static final String PLUS = "+";
+    public static final String ZERO = "0";
+}
--- src/main/java/com/everisbootcamp/accountdeposit/Constants/Paths/Path.java
+++ src/main/java/com/everisbootcamp/accountdeposit/Constants/Paths/Path.java
@@ -1,36 +1,34 @@
 package com.everisbootcamp.accountdeposit.Constants.Paths;
 
+import com.everisbootcamp.accountdeposit.Constants.Constan;
+
 public enum Path {
     ;
 
     public static class URLS {
 
-        private static final String DOUBLE_POINT = ":";
-        private static final String DOUBLE_SLASH = "//";
-        private static final String SLASH = "/";
-
         private static final String IPR = "localhost";
         private static final String PORT = "9500";
 
-        private static final String SERVER_PATH = SLASH.concat("server");
-        private static final String MS_PATH = SLASH.concat("microservices");
+        private static final String SERVER_PATH = Constan.SLASH.concat("server");
+        private static final String MS_PATH = Constan.SLASH.concat("microservices");
 
         private static final String HTTP_CONSTANT =
-            "http".concat(DOUBLE_POINT).concat(DOUBLE_SLASH);
-        private static final String GATEWAY = IPR.concat(DOUBLE_POINT).concat(PORT);
+            "http".concat(Constan.DOUBLE_POINT).concat(Constan.DOUBLE_SLASH);
+        private static final String GATEWAY = IPR.concat(Constan.DOUBLE_POINT).concat(PORT);
 
         private static final String SERVICE_CALL(String value) {
-            return HTTP_CONSTANT.concat(GATEWAY).concat(value).concat(SLASH);
+            return HTTP_CONSTANT.concat(GATEWAY).concat(value).concat(Constan.SLASH);
         }
 
         public static final String LOGIC_PATH = SERVICE_CALL(SERVER_PATH)
             .concat("logic")
-            .concat(SLASH);
+            .concat(Constan.SLASH);
         public static final String CUSTOMERS_PATH = SERVICE_CALL(MS_PATH)
             .concat("customer")
-            .concat(SLASH);
+            .concat(Constan.SLASH);
         public static final String ACCOUNT_PATH = SERVICE_CALL(MS_PATH)
             .concat("accounts")
-            .concat(SLASH);
+            .concat(Constan.SLASH);
     }
 }
