d444ffb8951be91a08feb47346a037b5808ad2cc
Kevin Anderson Palma Lluén <umb.kevsidorov@gmail.com>
2022-01-04 10:03:52 -0500

    REFACTOR: Add empty for string empty.

--- src/main/java/com/everisbootcamp/accountdeposit/Constants/Constan.java
+++ src/main/java/com/everisbootcamp/accountdeposit/Constants/Constan.java
@@ -3,11 +3,12 @@ package com.everisbootcamp.accountdeposit.Constants;
 public class Constan {
 
     public static final String FORMATTER_DATE = "dd-MM-yyyy";
-    public static final String DOUBLE_SLASH = "//";
     public static final String DOUBLE_SLASH_REVERSE = "\\";
+    public static final String DOUBLE_SLASH = "//";
     public static final String DOUBLE_POINT = ":";
-    public static final String SLASH = "/";
     public static final String SCRIPT = "-";
+    public static final String SLASH = "/";
     public static final String PLUS = "+";
     public static final String ZERO = "0";
+    public static final String EMPTY = "";
 }
